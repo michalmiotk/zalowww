bf426c702c0afc11e73a7b03bcd6d3c9ad427b1b
mm <michalmiotk95@gmail.com>
2021-02-11 22:37:52 +0100

    dziala proces zamowienia

--- code/accounts/models.py
+++ code/accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 
 from products.models import Product
@@ -9,7 +10,7 @@ class Profile(models.Model):
     ebooks = models.ManyToManyField(Product, blank=True)
 
     def __str__(self):
-        return self.user.name
+        return self.user.username
 
 def post_save_profile_create(sender, instance, created, *args, **kwargs):
     if created:
@@ -17,4 +18,3 @@ def post_save_profile_create(sender, instance, created, *args, **kwargs):
 
 
 post_save.connect(post_save_profile_create, sender=settings.AUTH_USER_MODEL)
-# Create your models here.
--- code/shopping_cart/views.py
+++ code/shopping_cart/views.py
@@ -4,7 +4,7 @@ from accounts.models import Profile
 from shopping_cart.models import OrderItem, Order
 from shopping_cart.extras import  generate_order_id
 from django.contrib.auth.decorators import login_required
-
+from django.contrib import messages
 # Create your views here.
 def get_user_pending_order(request):
     user_profile = get_object_or_404(Profile, user=request.user)
@@ -15,22 +15,28 @@ def get_user_pending_order(request):
 
 @login_required()
 def add_to_cart(request, **kwargs):
+    messages.info(request, "dooooo")
+    print("kwargs", kwargs)
+    print("request",  request, request.user)
     user_profile = get_object_or_404(Profile, user=request.user)
+    print("user_profile", user_profile)
+    
     product = Product.objects.filter(id=kwargs.get('item_id', "")).first()
+    print("request.user.profile.ebooks", request.user.profile.ebooks.all())
     if product in request.user.profile.ebooks.all():
         messages.info(request, "You already own this ebook")
-        return redirect(reverse('products:product-list'))
+        return redirect(reverse('product-list'))
     
     order_item, status = OrderItem.objects.get_or_create(product=product)
 
     user_order, status = Order.objects.get_or_create(owner=user_profile)
-    user.order.items.add(order_item)
+    user_order.items.add(order_item)
     if status:
         user_order.ref_code = generate_order_id()
         user_order.save()
-    
-    message.info(request, "item added to cart")
-    return redirect(reverse('products:product-list'))
+    user_profile.ebooks.add(product)
+    messages.info(request, "item added to cart")
+    return redirect(reverse('product-list'))
 
 @login_required
 def delete_from_cart(request, item_id):
@@ -38,7 +44,7 @@ def delete_from_cart(request, item_id):
     if item_to_delete.exists():
         item_to_delete[0].delete()
         messages.info(request, "Item has been deleted")
-    return redirect(reverse('shopping_cart:order_summary'))
+    return redirect(reverse('order_summary'))
 
 @login_required()
 def order_details(request, **kwargs):
@@ -47,3 +53,17 @@ def order_details(request, **kwargs):
         'order': existing_order
     }
     return render(request, 'shopping_cart/order_summary.html', context)
+
+@login_required
+def checkout(request, **kwargs):
+    context = {
+        'order' : get_user_pending_order(request)
+    }
+    return render(request, 'shopping_cart/checkout.html', context)
+
+@login_required
+def payment(request, order_id):
+    context = {
+        'order_id' : order_id
+    }
+    return render(request, 'shopping_cart/payment.html', context)
